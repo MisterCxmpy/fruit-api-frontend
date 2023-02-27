6189612610266532eb5aadd7bbafbbcf10dc4f5b
MisterCxmpy <cosgunhussein@gmail.com>
2023-02-27 16:08:48 +0000

    add pixabay api and image handle

--- script.js
+++ script.js
@@ -1,21 +1,95 @@
 const inputSection = document.querySelector("#inputSection form");
 const fruitList = document.querySelector("#fruitSection ul");
+const fruitNutrition = document.querySelector("#nutritionSection p");
+const fruitImage = document.querySelector("#fruitImage")
+
+const API_KEY = "<API-KEY>";
+let calories = 0;
 
 inputSection.addEventListener("submit", extractFruit);
 
 function extractFruit(e) {
   e.preventDefault();
   let fruitInput = e.target.fruitInput.value;
-  if (fruitInput) addFruit(fruitInput);
+  if (fruitInput) {
+    fetchFruitData(fruitInput);
+    fetchImgData(fruitInput);
+  }
   e.target.fruitInput.value = "";
 }
 
 function addFruit(fruit) {
   const li = document.createElement("li");
 
-  li.textContent = fruit;
+  li.textContent = fruit.name;
 
-  li.addEventListener("click", li.remove, { once: true });
+  li.addEventListener(
+    "click",
+    () => {
+      li.remove();
+      fruitNutrition.textContent = `Total calories: ${(calories -=
+        fruit.nutritions.calories)}`;
+    },
+    { once: true }
+  );
 
   fruitList.appendChild(li);
+
+  getCalories(fruit);
+}
+
+function addImage(image) {
+  fruitImage.src = image.hits[getRandomInt(image.hits.length)].previewURL;
+}
+
+function getCalories(fruit) {
+  fruitNutrition.textContent = `Total calories: ${(calories +=
+    fruit.nutritions.calories)}`;
+}
+
+async function fetchFruitData(fruit) {
+  try {
+    const resp = await fetch(`https://fruity-api.onrender.com/fruits/${fruit}`);
+    if (resp.ok) {
+      const data = await resp.json();
+      addFruit(data);
+    } else {
+      throw `Error: http status code = ${resp.status}`
+    }
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+async function fetchImgData(fruit) {
+  try {
+    const resp = await fetch(`https://pixabay.com/api/?q=${fruit}+fruit&key=${API_KEY}`);
+    if (resp.ok) {
+      const data = await resp.json();
+      addImage(data)
+    } else {
+      throw `Error: http status code = ${resp.status}`
+    }
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+// function fetchFruitData(fruit) {
+//   fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+//   .then((resp) => processResponse(resp))
+//     .then((data) => addFruit(data))
+//     .catch((e) => console.log(e));
+// }
+
+// function processResponse(resp) {
+//   if (resp.ok) {
+//     return resp.json()
+//   } else {
+//     throw `Error: http status code = ${resp.status}`
+//   }
+// }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
